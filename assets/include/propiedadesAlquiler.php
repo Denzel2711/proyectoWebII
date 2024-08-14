22444c064d1895b538a89b2a810e84e05bec0001
Denzel Herrera García <deherreraga@est.utn.ac.cr>
2024-08-14 11:11:35 -0600

    actualización

--- assets/include/propiedadesAlquiler.php
+++ assets/include/propiedadesAlquiler.php
@@ -1,4 +1,4 @@
-<section class="properties-for-rent bg-dark text-white py-5">
+<section class="properties-for-rent py-5" style="background-color: <?php mostrarColor1(); ?>; color: <?php mostrarColor2(); ?>;">
     <div class="container">
         <h2 class="text-center mb-5">PROPIEDADES EN ALQUILER</h2>
         <div class="row">
@@ -6,10 +6,10 @@
                 <div class="card" style="background-color: <?php mostrarColor1(); ?>;">
                     <img src="./assets/img/property1.jpeg" alt="Casa Mora">
                     <div class="card-body">
-                        <h5 class="card-title text-center" style="color:aliceblue">Casa Mora</h5>
-                        <p class="card-text text-center">Ubicada en las faldas del volcán Arenal, contiene 1000 mts de
+                        <h5 class="card-title text-center" style="color: <?php mostrarColor2(); ?>;">Casa Mora</h5>
+                        <p class="card-text text-center" style="color: <?php mostrarColor2(); ?>;">Ubicada en las faldas del volcán Arenal, contiene 1000 mts de
                             terreno y una de las mejores vistas del lugar</p>
-                        <p class="text-warning text-center">Precio: $65000</p>
+                        <p class="text-center" style="color: <?php mostrarColor3(); ?>;">Precio: $65000</p>
                         <div class="d-flex justify-content-center">
                             <a href="#" class="btn" id="vermas" style="border-color: <?php mostrarColor2(); ?>; color: <?php mostrarColor2(); ?>;" onmouseover="this.style.backgroundColor='<?php mostrarColor2(); ?>', this.style.color='<?php mostrarColor1(); ?>';" onmouseout="this.style.backgroundColor='transparent', this.style.color='<?php mostrarColor2(); ?>';">VER MÁS...</a>
                         </div>
@@ -17,13 +17,13 @@
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="card bg-dark">
+                <div class="card" style="background-color: <?php mostrarColor1(); ?>;">
                     <img src="./assets/img/property1.jpeg" alt="Casa Mora">
                     <div class="card-body">
-                        <h5 class="card-title text-center" style="color:aliceblue">Casa Mora</h5>
-                        <p class="card-text text-center">Ubicada en las faldas del volcán Arenal, contiene 1000 mts de
+                        <h5 class="card-title text-center" style="color: <?php mostrarColor2(); ?>;">Casa Mora</h5>
+                        <p class="card-text text-center" style="color: <?php mostrarColor2(); ?>;">Ubicada en las faldas del volcán Arenal, contiene 1000 mts de
                             terreno y una de las mejores vistas del lugar</p>
-                        <p class="text-warning text-center">Precio: $65000</p>
+                        <p class="text-center" style="color: <?php mostrarColor3(); ?>;">Precio: $65000</p>
                         <div class="d-flex justify-content-center">
                             <a href="#" class="btn" id="vermas" style="border-color: <?php mostrarColor2(); ?>; color: <?php mostrarColor2(); ?>;" onmouseover="this.style.backgroundColor='<?php mostrarColor2(); ?>', this.style.color='<?php mostrarColor1(); ?>';" onmouseout="this.style.backgroundColor='transparent', this.style.color='<?php mostrarColor2(); ?>';">VER MÁS...</a>
                         </div>
@@ -31,13 +31,13 @@
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="card bg-dark">
+                <div class="card" style="background-color: <?php mostrarColor1(); ?>;">
                     <img src="./assets/img/property1.jpeg" alt="Casa Mora">
                     <div class="card-body">
-                        <h5 class="card-title text-center" style="color:aliceblue">Casa Mora</h5>
-                        <p class="card-text text-center">Ubicada en las faldas del volcán Arenal, contiene 1000 mts de
+                        <h5 class="card-title text-center" style="color: <?php mostrarColor2(); ?>;">Casa Mora</h5>
+                        <p class="card-text text-center" style="color: <?php mostrarColor2(); ?>;">Ubicada en las faldas del volcán Arenal, contiene 1000 mts de
                             terreno y una de las mejores vistas del lugar</p>
-                        <p class="text-warning text-center">Precio: $65000</p>
+                        <p class="text-center" style="color: <?php mostrarColor3(); ?>;">Precio: $65000</p>
                         <div class="d-flex justify-content-center">
                             <a href="#" class="btn" id="vermas" style="border-color: <?php mostrarColor2(); ?>; color: <?php mostrarColor2(); ?>;" onmouseover="this.style.backgroundColor='<?php mostrarColor2(); ?>', this.style.color='<?php mostrarColor1(); ?>';" onmouseout="this.style.backgroundColor='transparent', this.style.color='<?php mostrarColor2(); ?>';">VER MÁS...</a>
                         </div>
--- assets/include/propiedadesDestacadas.php
+++ assets/include/propiedadesDestacadas.php
@@ -1,15 +1,15 @@
-<section class="featured-properties bg-dark text-white py-5">
+<section class="featured-properties py-5" style="background-color: <?php mostrarColor1(); ?>; color: <?php mostrarColor2(); ?>;">
     <div class="container">
         <h2 class="text-center mb-5">PROPIEDADES DESTACADAS</h2>
         <div class="row">
             <div class="col-md-4">
-                <div class="card bg-dark">
+                <div class="card" style="background-color: <?php mostrarColor1(); ?>;">
                     <img src="./assets/img/property1.jpeg" alt="Casa Mora">
                     <div class="card-body">
-                        <h5 class="card-title text-center" style="color:aliceblue">Casa Mora</h5>
-                        <p class="card-text text-center">Ubicada en las faldas del volcán Arenal, contiene 1000 mts de
+                        <h5 class="card-title text-center" style="color: <?php mostrarColor2(); ?>;">Casa Mora</h5>
+                        <p class="card-text text-center" style="color: <?php mostrarColor2(); ?>;">Ubicada en las faldas del volcán Arenal, contiene 1000 mts de
                             terreno y una de las mejores vistas del lugar</p>
-                        <p class="text-warning text-center">Precio: $65000</p>
+                        <p class="text-center" style="color: <?php mostrarColor3(); ?>;">Precio: $65000</p>
                         <div class="d-flex justify-content-center">
                             <a href="#" class="btn" id="vermas" style="border-color: <?php mostrarColor2(); ?>; color: <?php mostrarColor2(); ?>;" onmouseover="this.style.backgroundColor='<?php mostrarColor2(); ?>', this.style.color='<?php mostrarColor1(); ?>';" onmouseout="this.style.backgroundColor='transparent', this.style.color='<?php mostrarColor2(); ?>';">VER MÁS...</a>
                         </div>
@@ -17,13 +17,13 @@
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="card bg-dark">
+                <div class="card" style="background-color: <?php mostrarColor1(); ?>;">
                     <img src="./assets/img/property1.jpeg" alt="Casa Mora">
                     <div class="card-body">
-                        <h5 class="card-title text-center" style="color:aliceblue">Casa Mora</h5>
-                        <p class="card-text text-center">Ubicada en las faldas del volcán Arenal, contiene 1000 mts de
+                        <h5 class="card-title text-center" style="color: <?php mostrarColor2(); ?>;">Casa Mora</h5>
+                        <p class="card-text text-center" style="color: <?php mostrarColor2(); ?>;">Ubicada en las faldas del volcán Arenal, contiene 1000 mts de
                             terreno y una de las mejores vistas del lugar</p>
-                        <p class="text-warning text-center">Precio: $65000</p>
+                        <p class="text-center" style="color: <?php mostrarColor3(); ?>;">Precio: $65000</p>
                         <div class="d-flex justify-content-center">
                             <a href="#" class="btn" id="vermas" style="border-color: <?php mostrarColor2(); ?>; color: <?php mostrarColor2(); ?>;" onmouseover="this.style.backgroundColor='<?php mostrarColor2(); ?>', this.style.color='<?php mostrarColor1(); ?>';" onmouseout="this.style.backgroundColor='transparent', this.style.color='<?php mostrarColor2(); ?>';">VER MÁS...</a>
                         </div>
@@ -31,18 +31,18 @@
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="card bg-dark">
+                <div class="card" style="background-color: <?php mostrarColor1(); ?>;">
                     <img src="./assets/img/property1.jpeg" alt="Casa Mora">
                     <div class="card-body">
-                        <h5 class="card-title text-center" style="color:aliceblue">Casa Mora</h5>
-                        <p class="card-text text-center">Ubicada en las faldas del volcán Arenal, contiene 1000 mts de
+                        <h5 class="card-title text-center" style="color: <?php mostrarColor2(); ?>;">Casa Mora</h5>
+                        <p class="card-text text-center" style="color: <?php mostrarColor2(); ?>;">Ubicada en las faldas del volcán Arenal, contiene 1000 mts de
                             terreno y una de las mejores vistas del lugar</p>
-                        <p class="text-warning text-center">Precio: $65000</p>
+                        <p class="text-center" style="color: <?php mostrarColor3(); ?>;">Precio: $65000</p>
                         <div class="d-flex justify-content-center">
                             <a href="#" class="btn" id="vermas" style="border-color: <?php mostrarColor2(); ?>; color: <?php mostrarColor2(); ?>;" onmouseover="this.style.backgroundColor='<?php mostrarColor2(); ?>', this.style.color='<?php mostrarColor1(); ?>';" onmouseout="this.style.backgroundColor='transparent', this.style.color='<?php mostrarColor2(); ?>';">VER MÁS...</a>
                         </div>
                     </div>
                 </div>
+            </div>
         </div>
-    </div>
 </section>
