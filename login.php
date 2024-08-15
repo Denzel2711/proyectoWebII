712610d08168edd3cdd47938e2747db6bd81f440
Denzel Herrera García <deherreraga@est.utn.ac.cr>
2024-08-14 18:42:40 -0600

    Cambio de diseño del login

--- login.php
+++ login.php
@@ -1,3 +1,8 @@
+<?php
+include_once 'php/conexion.php';
+include_once 'php/mostrar.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,7 +10,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./style/login.css">
+    <link rel="stylesheet" href="./style/login.css?202406=<?php echo (rand()); ?>">
 </head>
 
 <body>
@@ -13,6 +18,19 @@
         <input type="checkbox" id="chk" aria-hidden="true">
 
         <div class="signup">
+            <form action="php/logicaLogin.php" method="post">
+                <label for="chk" aria-hidden="true">Iniciar</label>
+                <input type="email" name="correo" placeholder="Email" required="">
+                <input type="password" name="contrasena" placeholder="Password" required="">
+                <button type="submit" name="login">Iniciar Sesión</button>
+            </form>
+            <div style="text-align: center; padding-top:20px;">
+                <img style="border-radius: 10px;" src="<?php mostrarIconoPrincipal() ?>" alt="" width="130" height="110px">
+            </div>
+        </div>
+
+
+        <div class="login">
             <form action="php/logicaRegistro.php" method="post">
                 <label for="chk" aria-hidden="true">Registrarse</label>
                 <input type="text" name="nombre" placeholder="Nombre" required="">
@@ -21,16 +39,6 @@
                 <input type="email" name="correo" placeholder="Correo" required="">
                 <input type="password" name="contrasena" placeholder="Contraseña" required="">
                 <button type="submit" name="submit">Enviar</button>
-            </form>
-        </div>
-
-
-        <div class="login">
-            <form action="php/logicaLogin.php" method="post">
-                <label for="chk" aria-hidden="true">Iniciar</label>
-                <input type="email" name="correo" placeholder="Email" required="">
-                <input type="password" name="contrasena" placeholder="Password" required="">
-                <button type="submit" name="login">Iniciar Sesión</button>
             </form>
         </div>
 
